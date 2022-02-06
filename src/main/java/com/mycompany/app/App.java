ff9f7bd2f4986a47130d4dd7b15ea6be492f1732
Leon Schenk <989485+leonschenk@users.noreply.github.com>
2022-02-06 20:01:44 +0000

    Initial commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,10 +5,22 @@
 
 package com.mycompany.app;
 
+import java.util.List;
+
+import com.github.hypfvieh.bluetooth.DeviceManager;
+import com.github.hypfvieh.bluetooth.wrapper.BluetoothAdapter;
+
+import org.freedesktop.dbus.exceptions.DBusException;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws DBusException
     {
         System.out.println( "Hello Remote World!" );
+        DeviceManager devMan = DeviceManager.createInstance(true);
+        List<BluetoothAdapter> lijst = devMan.scanForBluetoothAdapters();
+        for (BluetoothAdapter adapter : lijst) {
+            System.out.println(adapter);
+        }
     }
 }
